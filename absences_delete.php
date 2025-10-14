ee046f2d81dad60e4fad52275df7916889ca5724
faizalqawi <faizalqawi09@gmail.com>
2025-10-14 10:58:54 +0700

    Add files via upload

---
+++ absences_delete.php
@@ -0,0 +1,25 @@
+<?php
+include 'config.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM absences WHERE id=$id";
+
+if ($koneksi->query($sql) === TRUE) {
+    echo "Data berhasil dihapus";
+} else {
+    echo "Error: " . $koneksi->error;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
